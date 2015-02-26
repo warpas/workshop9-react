8ec1829a6fb1960535d8c31fe72114b166dcc6b8
Dawid Warpas <dawid.warpas@gmail.com>
2015-02-26 16:03:41 +0100

    [#2] add Hello world in React without JSX

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 console.log("src/app.js reporting!");
+
+var React = require('react/addons');
+
+var App = React.createClass({displayName: "App",
+  render: function() {
+    return React.createElement("h3", null, "Hello, world!");
+  }
+});
+
+React.render(
+  React.createElement(App),
+  document.getElementById('slot')
+);
