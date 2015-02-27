343b263745d773615bf4c084eaa247c5578be63c
Dawid Warpas <dawid.warpas@gmail.com>
2015-02-27 17:50:34 +0100

    [#3] add simple Stories component

--- src/app.js
+++ src/app.js
@@ -4,8 +4,29 @@ var React = require('react/addons');
 
 var App = React.createClass({
   render: function() {
-    return <h3>Hello, world!</h3>;
+    return (
+      <div>
+        <h3>Hello, world!</h3>
+        <Stories source="https://fierce-gorge-1132.herokuapp.com/stories" />
+      </div>
+    );
   }
+});
+
+var Stories = React.createClass({
+  getInitialState: function() {
+    return {
+      all: []
+    };
+  },
+
+  render: function() {
+    return (
+      <div className="stories">
+        <h4>Stories:</h4>
+      </div>
+    );
+  },
 });
 
 React.render(<App />, document.getElementById('slot'));
